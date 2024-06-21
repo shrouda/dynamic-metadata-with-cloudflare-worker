9d35be9fb05c4c5e8058947c6a1416f9c9ef2f71
shrouda <49821910+shrouda@users.noreply.github.com>
2024-06-21 14:11:28 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
   domainSource: "https://adcccaa3-ef16-48c0-975b-dad539ddf9d2.weweb-preview.io/", // Your WeWeb app link
-  metaDataEndpoint: "https://x8e3-4uyz-scg3.n7c.xano.io/api:eabalGiE/get_single_article_wordpress_meta", // Link of the endpoint that returns the metadata. /{id} will be added to this path. With id being the last parameter of the dynamic page
+  metaDataEndpoint: "https://x8e3-4uyz-scg3.n7c.xano.io/api:eabalGiE/get_single_article_wordpress_meta/{id}", // Link of the endpoint that returns the metadata. /{id} will be added to this path. With id being the last parameter of the dynamic page
   patterns: {
     dynamicPage: "/article/[^/]+"
   }
